ede3f7f83e6959be57c5400c4f4ff60dfe025269
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2024-01-18 15:55:35 +0200

    Part 1 Exercise 4

--- 1/kurssitiedot/src/App.jsx
+++ 1/kurssitiedot/src/App.jsx
@@ -35,24 +35,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1.name, part1.exercises, part2.name, part2.exercises, part3.name, part2.exercises]}/>
-      <Total exercises={[part1.exercises, part2.exercises, part2.exercises]} />
+      <Content parts={[parts[0].name, parts[0].exercises, parts[1].name, parts[1].exercises, parts[2].name, parts[2].exercises]}/>
+      <Total exercises={[parts[0].exercises, parts[1].exercises, parts[2].exercises]} />
     </div>
   )
 }
