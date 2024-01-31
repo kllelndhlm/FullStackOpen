a16612a3e904196d9f23c4ef1f63a9e5ac2e4c96
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2024-01-31 14:30:20 +0200

    Part 2 Exercise 11

--- 2/puhelinluettelo/src/App.jsx
+++ 2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Names = (props) => {
   return (
@@ -44,35 +45,41 @@ const Filter = (props) => {
 }
 
 const App = () => {
-  const [names, setNames] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [newFilter, setNewFilter] = useState('')
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
   const addName = (event) => {
     event.preventDefault()
-    if (names.map(name => name.name).includes(newName)) 
+    if (persons.map(name => name.name).includes(newName)) 
       return (
         alert(`${newName} is already added to phonebook`)
       )
     const nameObject = {
       name: newName,
       number: newNumber,
-      id: names.length + 1
+      id: persons.length + 1
     }
   
-    setNames(names.concat(nameObject))
+    setPersons(persons.concat(nameObject))
     setNewName('')
     setNewNumber('')
   }
 
-  const filteredNames = names.filter(name => name.name.toLowerCase().includes(newFilter.toLowerCase()) === true)
+  const filteredNames = persons.filter(name => name.name.toLowerCase().includes(newFilter.toLowerCase()) === true)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
